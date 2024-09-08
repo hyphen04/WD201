58f9f61ffac97498705d8fb579db6fb37c0a4489
Kunj Patel <kunj2604@gmail.com>
2024-09-08 18:44:58 +0530

    bcryptjs added

--- todo-app/app.js
+++ todo-app/app.js
@@ -10,7 +10,7 @@ const connectEnsureLogin = require("connect-ensure-login");
 const session = require("express-session");
 const LocalStrategy = require("passport-local");
 const flash = require("connect-flash");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 const saltRounds = 10;
 app.set("views", path.join(__dirname, "views"));
